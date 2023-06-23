a638aed0711e5d1e54a7c1de4409a1a0a83b4dce
manuelmolano <manuelmolanomazon@gmail.com>
2023-06-23 17:03:57 +0200

    test task_template

--- ngym_gridworld/envs/task_template.py
+++ ngym_gridworld/envs/task_template.py
@@ -5,16 +5,30 @@ Created on Fri June 16 15:44:59 2023
 
 @author: molano
 """
+import numpy as np
 from gym import spaces
 import neurogym as ngym
 
-class YourTask(ngym.TrialEnv):
-    metadata = {}
+class GridWorld(ngym.TrialEnv):
+    """GridWorld task.
 
-    def __init__(self, dt=100, timing=None, extra_input_param=None):
+    Args:
+        cohs: list of float, coherence levels controlling the difficulty of
+            the task
+        sigma: float, input noise level
+        dim_ring: int, dimension of ring input and output
+    """
+    metadata = {
+        'paper_link': '',
+        'paper_name': '',
+        'tags': ['perceptual', 'two-alternative', 'supervised']
+    }
+    def __init__(self, dt=100):
         super().__init__(dt=dt)
-       
-
+        name = {'fixation': 0, 'stimulus': range(1, 9)}
+        self.observation_space = spaces.Box(
+            -np.inf, np.inf, shape=(9,), dtype=np.float32)
+        self.action_space = spaces.Discrete(5)
     def new_trial(self, **kwargs):
         """
         new_trial() is called when a trial ends to generate the next trial.
@@ -34,6 +48,15 @@ class YourTask(ngym.TrialEnv):
                 ground truth correct response, info['gt']
                 boolean indicating the end of the trial, info['new_trial']
         """
-
+        obs = 0
+        reward = 0
+        done = False
+        new_trial = False
+        gt = 0
         return obs, reward, done, {'new_trial': new_trial, 'gt': gt}
 
+
+if __name__ == '__main__':
+   task = YourTask(dim_ring=4)
+   task.reset()
+   print(task)
